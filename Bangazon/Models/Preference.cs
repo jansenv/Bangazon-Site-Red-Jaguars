2ed6db67e2c6f8a333dfad202de830cbd3b9a1ea
jansenv <jansenv44@gmail.com>
2020-04-27 17:21:19 -0500

    created model

---
+++ Bangazon/Models/Preference.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangazon.Models
+{
+    public class Preference
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public int PostId { get; set; }
+
+        public bool? LikeOrDislike { get; set; }
+    }
+}
